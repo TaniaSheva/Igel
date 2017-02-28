b1723af5bed7af662d129570aadd68a040f408fb
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-02-28 15:31:05 +0200

    Homework1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,15 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 
+		System.out.println("Hello World");
+		
+		int a;
+		a=2*2;
+		
+		int b;
+		b=a*10;
+		
+		System.out.println(a*b+b+10);
 		
 	}
 
