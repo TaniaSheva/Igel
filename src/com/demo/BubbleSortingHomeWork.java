541e81183996077e554fa00f0555743abec811cb
Tanya <Tanya@air-tanya>
2017-02-28 23:09:49 +0200

    Home work 2 done - added finding element by binary search

--- src/com/demo/BubbleSortingHomeWork.java
+++ src/com/demo/BubbleSortingHomeWork.java
@@ -4,7 +4,39 @@ public class BubbleSortingHomeWork {
 
 	public static void main(String[] args) {
 		int array [] = {12,1,4,6,16,22,15,23,45,90};
+		sortArray(array);
+		printArray(array);
+		
+		boolean contain = doesContainNumber(array, 4);
+		System.out.println("Does array contain inserted value? " + contain);
+	}
 	
+	//to find out if the array contains inserted number
+	public static boolean doesContainNumber(int[] array, int toFindElement)
+	{	
+		int leftIndex = 0;
+		int rightIndex = array.length - 1;
+		
+		while (leftIndex <= rightIndex) {
+			
+		int midIndex = leftIndex + (rightIndex - leftIndex)/2;
+		int midElement = array[midIndex];
+		if (toFindElement == midElement) {
+			return true;
+		}
+		else if (toFindElement > midElement) {
+			leftIndex = midIndex + 1;
+		}
+		else {
+			rightIndex = midIndex - 1;
+		}
+		}
+		return false;
+	}
+	
+	//to sort array
+	public static void sortArray(int[] array)
+	{
 		for (int indexTo = 0; indexTo < array.length - 1; indexTo++) {
 			
 			int indexFrom = indexTo;
@@ -19,14 +51,16 @@ public class BubbleSortingHomeWork {
 			//switching elements
 			int buffer = array[indexTo];
 			array[indexTo] = array[indexFrom];
-			array[indexFrom] = buffer;
-			
+			array[indexFrom] = buffer;	
 		}
-		
-		for (int i = 0; i < array.length; i++) {
-			System.out.print(array[i] + " ");
-		}
-	
 	}
 
+	public static void printArray(int[] array)
+	{	System.out.println("Sorted array: ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+			
+		}
+		System.out.println();
+	}
 }
