fa8f75cf2f9b190eccf1654cf44b859082a48e7a
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-02-28 15:41:34 +0200

    Homework1 added correct values

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -3,14 +3,12 @@ public class HomeWork1 {
 
 	public static void main(String[] args) {
 		double a = 2;
-		double b = 4;
-		double c = 7;
+		double b = -8;
+		double c = -24;
 		
-		double x1;
-		double x2;
+		double x1 = (-b + Math.sqrt(b*b-4*a*c))/(2*a);
+		double x2 = (-b - Math.sqrt(b*b-4*a*c))/(2*a);
 		
-		x1 = (-b + Math.sqrt(b*b-4*a*c))/(2*a);
-		x2 = (-b - Math.sqrt(b*b-4*a*c))/(2*a);
 		System.out.println(x1);
 		System.out.println(x2);
 
