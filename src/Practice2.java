220074e940fd79f068a4c05e61b9f0208fe7887f
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-03-01 12:00:39 +0200

    Changed practice 2

--- src/Practice2.java
+++ src/Practice2.java
@@ -3,7 +3,7 @@ public class Practice2 {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {			
-			for (int j=1; j<=25; j++) // j<N+1, N=5
+			for (int j=1; j<6; j++) // j<N+1, N=5
             {
                   if (i == j) {
                          System.out.print("*");
