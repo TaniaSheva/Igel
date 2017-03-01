6ec4c09835d212f36ff62a18226c3292302c31fa
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-03-01 11:56:14 +0200

    Module2-3

---
+++ src/Module3/Practice.java
@@ -0,0 +1,10 @@
+package Module3;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Practice2.java
@@ -0,0 +1,25 @@
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {			
+			for (int j=1; j<=25; j++) // j<N+1, N=5
+            {
+                  if (i == j) {
+                         System.out.print("*");
+                  }
+                  
+                  else if (i + j == 6) { // i + j = N+1, N = 5
+                      System.out.print("*");
+                  }
+                  
+                  else {
+                         System.out.print(j);
+                  }
+    
+				}
+				System.out.println();
+		}
+
+	}
+}
