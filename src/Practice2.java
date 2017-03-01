4ca0fd1f2a16b5d1abaa4c0a6ac48f5e7211bd0a
Tetiana_Slukhai <Tetiana_Slukhai@epam.com>
2017-03-01 12:50:39 +0200

    Formatted data in Practice 2

--- src/Practice2.java
+++ src/Practice2.java
@@ -2,21 +2,25 @@
 public class Practice2 {
 
 	public static void main(String[] args) {
+		int number = 1;
 		for (int i = 1; i <= 5; i++) {			
 			for (int j=1; j<6; j++) // j<N+1, N=5
-            {
+            {		
+				
                   if (i == j) {
-                         System.out.print("*");
+                         System.out.print("*	");
                   }
                   
                   else if (i + j == 6) { // i + j = N+1, N = 5
-                      System.out.print("*");
+                      System.out.print("*	");
                   }
                   
                   else {
-                         System.out.print(j);
+                	     
+                         System.out.printf("%02d",number);
+                         System.out.print("	");
                   }
-    
+                  number++;
 				}
 				System.out.println();
 		}
