638a9e734b81798dacee04e209449f96b9f0543c
Tanya <Tanya@air-tanya>
2017-03-26 20:48:17 +0300

    Added time observer removal

--- src/HomeWork4/UseTimeServer.java
+++ src/HomeWork4/UseTimeServer.java
@@ -1,16 +1,19 @@
 package HomeWork4;
 
 public class UseTimeServer {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		TimeServer timeServer1 = new TimeServer(10); //first implementation
-		TimeServer timeServer2 = new TimeServer(5); //second implementation
+		TimeServer timeServer1 = new TimeServer(5); //first implementation
+		TimeServer timeServer2 = new TimeServer(10); //second implementation
 		
 		TimeObserver timeObserver1 = new TimeObserver("First observer");
 		TimeObserver timeObserver2 = new TimeObserver("Second observer");
 		
 		timeServer1.addObserver(timeObserver1);
 		timeServer2.addObserver(timeObserver2);
+		Thread.sleep(15000);
+		timeServer1.removeObserver(timeObserver1); //встигає двічі нотіфайнутись і видаляється
+		timeServer2.removeObserver(timeObserver2); //встигає нотіфайнутись 1 раз і видаляється
 	}
 
 }
